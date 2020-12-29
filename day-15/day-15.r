2df0e7883c08be2b9f86fa9a49880d2923eb1336
alexvornsand <ajvornsand@ucdavis.edu>
2020-12-29 09:23:46 -0800

    Update day-15.r

--- day-15/day-15.r
+++ day-15/day-15.r
@@ -2,18 +2,30 @@
 # day 15
 
 # part 1
-gameNumbers <- c(9, 12, 1, 4, 17, 0, 18)
+gameNumbers <- list(
+  '9' = 1,
+  '12' = 2,
+  '1' = 3,
+  '4' = 4,
+  '17' = 5,
+  '0' = 6,
+  '18' = 7
+)
+last <- 18
 
-nextGameNumber <- function(gameNumbers = gameNumbers){
-  if(length(which(gameNumbers == gameNumbers[length(gameNumbers)])) == 1){
+nextGameNumber <- function(last, gameNumbers = gameNumbers){
+  if(length(gameNumbers[[as.character(last)]]) == 1){
     return(0)
   } else {
-    return(length(gameNumbers) - which(gameNumbers == gameNumbers[length(gameNumbers)])[length(which(gameNumbers == gameNumbers[length(gameNumbers)])) - 1])
+    gameNumbers[[as.character(last)]] <- rev(rev(gameNumbers[[as.character(last)]])[1:2])
+    lastTimes <- rev(gameNumbers[[as.character(last)]])
+    return(lastTimes[1] - lastTimes[2])
   }
 }
 
-for(i in 1:30000000){
-  gameNumbers <- c(gameNumbers, nextGameNumber(gameNumbers))
+for(i in 8:30000000){
+  if(i %% 10000 == 0){print(i)}
+  nextNumber <- nextGameNumber(last, gameNumbers)
+  gameNumbers[[as.character(nextNumber)]] <- c(gameNumbers[[as.character(nextNumber)]], i)
+  last <- nextNumber
 }
-
-gameNumbers[2020]
