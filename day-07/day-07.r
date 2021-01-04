168c998cdb1375a4017e6e3de16fb0e68e7b555e
alexvornsand <ajvornsand@ucdavis.edu>
2021-01-04 15:45:05 -0800

    Update day-07.r

--- day-07/day-07.r
+++ day-07/day-07.r
@@ -2,44 +2,54 @@
 # day 7
 
 # part 1
-bagRules <- readLines('day-7.txt')
+library(tictoc)
 
-ruleList <- list()
+input <- readLines('day-07.txt')
 
-for(rule in bagRules){
-  keyValue <- trimws(unlist(strsplit(gsub('bag', '', gsub('bags', 'bag', rule)), split = ' contain ')))
-  bags <- trimws(gsub('.', '', unlist(strsplit(keyValue[2], split = ',')), fixed = T))
-  if(bags[1] == 'no other'){
-    ruleList[[keyValue[1]]] <- 'no other'
-  } else {
-    ruleList[[keyValue[1]]] <- list()
-    for(bag in bags){
-      bagCount <- as.integer(substring(bag, 1, 1))
-      bagId <- trimws(substring(bag, 3, nchar(bag)))
-      ruleList[[keyValue[1]]][[bagId]] <- bagCount
+countBags <- function(input, partTwo = F){
+  ruleList <- list()
+  for(rule in input){
+    keyValue <- trimws(unlist(strsplit(gsub('bag', '', gsub('bags', 'bag', rule)), split = ' contain ')))
+    bags <- trimws(gsub('.', '', unlist(strsplit(keyValue[2], split = ',')), fixed = T))
+    if(bags[1] == 'no other'){
+      ruleList[[keyValue[1]]] <- 'no other'
+    } else {
+      ruleList[[keyValue[1]]] <- list()
+      for(bag in bags){
+        bagCount <- as.integer(substring(bag, 1, 1))
+        bagId <- trimws(substring(bag, 3, nchar(bag)))
+        ruleList[[keyValue[1]]][[bagId]] <- bagCount
+      }
     }
   }
-}
-
-checkForShinyGold <- function(bag){
-  if(any(names(ruleList[[bag]]) == 'shiny gold')){
-    return(T)
-  } else if(ruleList[[bag]][1] == 'no other'){
-    return(F)
+  if(partTwo == T){
+    countBagContents <- function(bag){
+      if(ruleList[bag] == 'no other'){
+        return(0)
+      } else {
+        return(sum(unlist(sapply(names(ruleList[[bag]]), function(x){return(ruleList[[bag]][[x]] * (1 + countBagContents(x)))}))))
+      }
+    }
+    return(countBagContents('shiny gold'))
   } else {
-    return(any(unlist(sapply(names(ruleList[[bag]]), checkForShinyGold))))
+    checkForShinyGold <- function(bag){
+      if(any(names(ruleList[[bag]]) == 'shiny gold')){
+        return(T)
+      } else if(ruleList[[bag]][1] == 'no other'){
+        return(F)
+      } else {
+        return(any(unlist(sapply(names(ruleList[[bag]]), checkForShinyGold))))
+      }
+    }
+    return(sum(unlist(sapply(names(ruleList), checkForShinyGold))))
   }
 }
 
-sum(unlist(sapply(names(ruleList), checkForShinyGold)))
+tictoc::tic()
+countBags(input)
+tictoc::toc()
 
 # part 2
-countBags <- function(bag){
-  if(ruleList[bag] == 'no other'){
-    return(0)
-  } else {
-    return(sum(unlist(sapply(names(ruleList[[bag]]), function(x){return(ruleList[[bag]][[x]] * (1 + countBags(x)))}))))
-  }
-}
-
-countBags('shiny gold')
+tictoc::tic()
+countBags(input, partTwo = T)
+tictoc::toc()
