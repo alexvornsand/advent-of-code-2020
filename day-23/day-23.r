0ca980438df45c2d1d8a0e47edbcc2f07d117ae9
alexvornsand <ajvornsand@ucdavis.edu>
2021-01-03 18:04:38 -0800

    Update day-23.r

--- day-23/day-23.r
+++ day-23/day-23.r
@@ -4,59 +4,51 @@
 # part 1
 input <- '135468729'
 
-predictCups <- function(input, k = 100, l = 9){
+predictCups <- function(input, partTwo = F){
+  if(partTwo == T){
+    k = 10000000
+    l = 1000000
+  } else {
+    k = 100
+    l = 9
+  }
   sequence <- 1:l
   header <- as.integer(unlist(strsplit(input, '')))
   sequence[1:length(header)] <- header
   max <- max(sequence)
+  nextSequence <- rep(NA, k)
+  nextSequence[sequence[1:(max - 1)]] <- sequence[-1]
+  nextSequence[sequence[max]] <- sequence[1]
+  current <- sequence[1]
   for(i in 1:k){
-    if(i %% 1000 == 0){print(i)}
-    current <- sequence[1]
-    removed <- sequence[2:4]
+    removed1 <- nextSequence[current]
+    removed2 <- nextSequence[removed1]
+    removed3 <- nextSequence[removed2]
     if(current == 1){
       target <- max
     } else {
       target <- current - 1
     }
-    while(target %in% removed){
+    while(target %in% c(removed1, removed2, removed3)){
       if(target == 1){
         target <- max
       } else {
         target <- target - 1
       }
     }
-    if(which(sequence == target) < length(sequence)){
-      post <- sequence[(which(sequence == target) + 1):length(sequence)]
-    } else {
-      post <- integer()
-    }
-    sequence <- c(sequence[5:which(sequence == target)], removed, post, current)
+    nextSequence[current] <- nextSequence[removed3]
+    current <- nextSequence[removed3]
+    nextSequence[removed3] <- nextSequence[target]
+    nextSequence[target] <- removed1
   }
-  if(k == 100 & l == 9){
-    if(which(sequence == 1) == 1){
-      post <- integer()
-    } else {
-      post <- sequence[1:(which(sequence == 1) - 1)]
-    }
-    if(which(sequence == 1) == length(sequence)){
-      pre <- integer()
-    } else {
-      pre <- sequence[(which(sequence == 1) + 1):length(sequence)]
-    }
-    return(paste(c(pre, post), collapse = ''))
+  if(partTwo == T){
+    return(prod(nextSequence[1], nextSequence[nextSequence[1]]))
   } else {
-    if(which(sequence == 1) == 1){
-      post <- integer()
-    } else {
-      post <- sequence[1:(which(sequence == 1) - 1)]
+    result <- nextSequence[1]
+    for(i in 2:8){
+      result <- c(result, nextSequence[tail(result, 1)])
     }
-    if(which(sequence == 1) == length(sequence)){
-      pre <- integer()
-    } else {
-      pre <- sequence[(which(sequence == 1) + 1):length(sequence)]
-    }
-    sequence <- c(pre, post)
-    return(as.character(prod(sequence[1:2])))
+    return(paste(result, collapse = ''))
   }
 }
 
@@ -64,4 +56,4 @@ predictCups(input)
 
 # part 2
 
-predictCups(input, k = 10000000, l = 1000000)
+predictCups(input, partTwo = T)
