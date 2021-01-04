513d7cc6bef9bbb1b36e83f08349cbe211acb9d6
alexvornsand <ajvornsand@ucdavis.edu>
2021-01-04 13:17:14 -0800

    Update day-02.r

--- day-02/day-02.r
+++ day-02/day-02.r
@@ -2,23 +2,28 @@
 # day 2
 
 # part 1
-passwordData <- readLines('day-2.txt')
+library(tictoc)
+input <- readLines('day-02.txt')
 
-evaluatePassword <- function(pw){
-  pwData <- read.table(text = gsub('-', '\t', gsub(':', '', gsub(' ', '\t', pw))), col.names = c('min', 'max', 'character', 'password'))
-  pwChars <- unlist(strsplit(pwData$password, split = ''))
-  count <- sum(pwChars == pwData$character)
-  return(count >= pwData$min & count <= pwData$max)
+countValidPasswords <- function(passwordData, partTwo = F){
+  evaluatePassword <- function(pw, partTwo){
+    pwData <- read.table(text = gsub('-', '\t', gsub(':', '', gsub(' ', '\t', pw))), col.names = c('min', 'max', 'character', 'password'))
+    pwChars <- unlist(strsplit(pwData$password, split = ''))
+    if(partTwo == F){
+      count <- sum(pwChars == pwData$character)
+      return(count >= pwData$min & count <= pwData$max)
+    } else {
+      return(sum(c(pwData$character == pwChars[pwData$min], pwData$character == pwChars[pwData$max])) == 1)
+    }
+  }
+  return(sum(sapply(passwordData, evaluatePassword, partTwo = partTwo)))
 }
 
-sum(sapply(passwordData, evaluatePassword))
+tictoc::tic()
+countValidPasswords(input)
+tictoc::toc()
 
 # part 2
-
-evaluatePassword <- function(pw){
-  pwData <- read.table(text = gsub('-', '\t', gsub(':', '', gsub(' ', '\t', pw))), col.names = c('min', 'max', 'character', 'password'))
-  pwChars <- unlist(strsplit(pwData$password, split = ''))
-  return(sum(c(pwData$character == pwChars[pwData$min], pwData$character == pwChars[pwData$max])) == 1)
-}
-
-sum(sapply(passwordData, evaluatePassword))
+tictoc::tic()
+countValidPasswords(passwordData, partTwo = T)
+tictoc::toc()
