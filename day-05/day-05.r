b7f907e0444dbde4bb66a6e84033a049072af90d
alexvornsand <ajvornsand@ucdavis.edu>
2021-01-04 14:59:20 -0800

    Update day-05.r

--- day-05/day-05.r
+++ day-05/day-05.r
@@ -3,29 +3,35 @@
 
 # part 1
 library(compositions)
-boardingPasses <- readLines('day-5.txt')
+library(tictoc)
 
-getSeatID <- function(boardingPass){
-  row <- unbinary(substring(gsub('B', '1', gsub('F', '0', boardingPass)), 1, 7))
-  column <- unbinary(substring(gsub('R', '1', gsub('L', '0', boardingPass)), 8, 10))
-  return(8 * row + column)
+input <- readLines('day-05.txt')
+
+findSeatID <- function(input, partTwo = F){
+  getSeatID <- function(boardingPass){
+    row <- unbinary(substring(gsub('B', '1', gsub('F', '0', boardingPass)), 1, 7))
+    column <- unbinary(substring(gsub('R', '1', gsub('L', '0', boardingPass)), 8, 10))
+    return(8 * row + column)
+  }
+  if(partTwo == T){
+    seats <- apply(do.call(expand.grid, list('row' = 1:128, 'column' = 0:7)), 1, function(x){return(x[[1]] * 8 + x[[2]])})
+    takenSeats <- unlist(sapply(input, getSeatID))
+    for(seat in seats){
+      if(!(seat %in% takenSeats) & (seat + 1) %in% takenSeats & (seat - 1) %in% takenSeats){
+        return(seat)
+        break
+      }
+    }
+  } else {
+    return(max(unlist(sapply(input, getSeatID))))
+  }
 }
 
-max(unlist(sapply(boardingPasses, getSeatID)))
+tictoc::tic()
+findSeatID(input)
+tictoc::toc()
 
 # part 2
-seats <- c()
-for(r in 1:128){
-  for(c in 0:7){
-    seats <- c(seats, (r * 8) + c)
-  }
-}
-
-takenSeats <- unlist(sapply(boardingPasses, getSeatID))
-
-for(seat in seats){
-  if(!(seat %in% takenSeats) & (seat + 1) %in% takenSeats & (seat - 1) %in% takenSeats){
-    print(seat)
-    break
-  }
-}
+tictoc::tic()
+findSeatID(input, partTwo = T)
+tictoc::toc()
