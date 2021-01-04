cdc2b0a810a66384c93388d1f22830d21a6ed84f
alexvornsand <ajvornsand@ucdavis.edu>
2021-01-04 13:06:45 -0800

    Update day-01.r

--- day-01/day-01.r
+++ day-01/day-01.r
@@ -2,24 +2,38 @@
 # day 1
 
 # part 1
-numbers <- scan('day-1.txt')
+library(tictoc)
+numbers <- scan('day-01.txt')
 
-for(i in 1:length(numbers)){
-  for(j in i:length(numbers)){
-    if(numbers[i] + numbers[j] == 2020){
-      print(numbers[i] * numbers[j])
+findProduct <- function(numbers, partTwo = F){
+  if(partTwo == F){
+    for(i in 1:length(numbers)){
+      for(j in i:length(numbers)){
+        if(numbers[i] + numbers[j] == 2020){
+          return(numbers[i] * numbers[j])
+        }
+      }
     }
-  }
-}
-
-# part 2
-
-for(i in 1:length(numbers)){
-  for(j in i:length(numbers)){
-    for(k in j:length(numbers)){
-      if(numbers[i] + numbers[j] + numbers[k] == 2020){
-        print(numbers[i] * numbers[j] * numbers[k])
+  } else {
+    for(i in 1:length(numbers)){
+      for(j in i:length(numbers)){
+        for(k in j:length(numbers)){
+          if(numbers[i] + numbers[j] + numbers[k] == 2020){
+            print(numbers[i] * numbers[j] * numbers[k])
+          }
+        }
       }
     }
   }
 }
+
+tictoc::tic()
+findProduct(numbers)
+tictoc::toc()
+
+# part 2
+tictoc::tic()
+findProduct(numbers, partTwo = T)
+tictoc::toc()
+
+
