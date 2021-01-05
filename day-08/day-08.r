9b68eec43565511a638c8f9eee324908516abd55
alexvornsand <ajvornsand@ucdavis.edu>
2021-01-04 18:06:10 -0800

    Update day-08.r

--- day-08/day-08.r
+++ day-08/day-08.r
@@ -2,67 +2,71 @@
 # day 8
 
 # part 1
-instructions <- readLines('day-8.txt')
+libraru(tictoc)
 
-testInstructions <- function(instructions, returnAcc = F){
-  index <- 1
-  instructionsRun <- c()
-  accumulator <- 0
-  while(index <= length(instructions)){
-    if(index %in% instructionsRun | index < 1){
-      if(returnAcc == T){
-        return(paste(F, accumulator, sep = ': '))
+input <- readLines('day-08.txt')
+
+findAccumulator <- function(input, partTwo = F){
+  testInput <- function(input){
+    index <- 1
+    inputRun <- c()
+    accumulator <- 0
+    while(index <= length(input)){
+      if(index %in% inputRun){
+        return(list('result' = F, 'acc' = accumulator))
       } else {
-        return(F)
-      }
-      break
-    } else {
-      if(substr(instructions[index], 1, 3) == 'nop') {
-        instructionsRun <- c(instructionsRun, index)
-        index <- index + 1
-      } else if (substr(instructions[index], 1, 3) == 'acc') {
-        if(substr(instructions[index], 5, 5) == '+'){
-          accumulator <- accumulator + as.integer(substr(instructions[index], 6, nchar(instructions[index])))
+        if(substr(input[index], 1, 3) == 'nop') {
+          inputRun <- c(inputRun, index)
+          index <- index + 1
+        } else if (substr(input[index], 1, 3) == 'acc') {
+          if(substr(input[index], 5, 5) == '+'){
+            accumulator <- accumulator + as.integer(substr(input[index], 6, nchar(input[index])))
+          } else {
+            accumulator <- accumulator - as.integer(substr(input[index], 6, nchar(input[index])))
+          }
+          inputRun <- c(inputRun, index)
+          index <- index + 1
         } else {
-          accumulator <- accumulator - as.integer(substr(instructions[index], 6, nchar(instructions[index])))
+          inputRun <- c(inputRun, index)
+          if(substr(input[index], 5, 5) == '+'){
+            index <- index + as.integer(substr(input[index], 6, nchar(input[index])))
+          } else {
+            index <- index - as.integer(substr(input[index], 6, nchar(input[index])))
+          }
         }
-        instructionsRun <- c(instructionsRun, index)
-        index <- index + 1
-      } else {
-        instructionsRun <- c(instructionsRun, index)
-        if(substr(instructions[index], 5, 5) == '+'){
-          index <- index + as.integer(substr(instructions[index], 6, nchar(instructions[index])))
-        } else {
-          index <- index - as.integer(substr(instructions[index], 6, nchar(instructions[index])))
+      }
+    }
+    return(list('result' = T, 'acc' = accumulator))
+  }
+  if(partTwo == F){
+    return(testInput(input)[['acc']])
+  } else {
+    for(i in 1:length(input)){
+      trialInput <- input
+      if(substr(trialInput[i], 1, 3) == 'jmp'){
+        substr(trialInput[i], 1, 3) <- 'nop'
+        trialResults <- testInput(trialInput)
+        if(trialResults[['result']] == T){
+          return(trialResults[['acc']])
+        }
+      } else if(substr(trialInput[i], 1, 3) == 'nop'){
+        substr(trialInput[i], 1, 3) <- 'jmp'
+        trialResults <- testInput(trialInput)
+        if(trialResults[['result']] == T){
+          return(trialResults[['acc']])
         }
       }
     }
   }
-  if(returnAcc == T){
-    return(paste(T, accumulator, sep = ': '))
-  } else {
-    return(T)
-  }
 }
 
-testInstructions(instructions)
+tictoc::tic()
+findAccumulator(input)
+tictoc::toc()
 
 # part 2
-for(i in 1:length(instructions)){
-  trialInstructions <- instructions
-  if(substr(trialInstructions[i], 1, 3) == 'jmp'){
-    substr(trialInstructions[i], 1, 3) <- 'nop'
-    if(testInstructions(trialInstructions) == T){
-      print(testInstructions(trialInstructions, T))
-      break
-    }
-  } else if(substr(trialInstructions[i], 1, 3) == 'nop'){
-    substr(trialInstructions[i], 1, 3) <- 'jmp'
-    if(testInstructions(trialInstructions) == T){
-      print(testInstructions(trialInstructions, T))
-      break
-    }
-  }
-}
+tictoc::tic()
+findAccumulator(input, partTwo = T)
+tictoc::toc()
 
 
