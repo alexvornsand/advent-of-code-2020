ac60e7ec8f5d320a7f07ff13d8bd89c6c144894f
alexvornsand <ajvornsand@ucdavis.edu>
2021-01-05 09:44:17 -0800

    Update day-12.r

--- solutions/day-12/day-12.r
+++ solutions/day-12/day-12.r
@@ -2,87 +2,86 @@
 # day 12
 
 # part 1
-directions <- readLines('day-12.txt')
+library(microbenchmark)
 
+input <- readLines('day-12.txt')
 
-posx <- 0
-posy <- 0
-r <- 0
-for(direction in directions){
-  action <- unlist(strsplit(direction, split = ''))[1]
-  magnitude <- as.integer(paste(unlist(strsplit(direction, split = ''))[2:nchar(direction)], collapse = ''))
-  if(action == 'N'){
-    posy <- posy + magnitude
-  } else if(action == 'S'){
-    posy <- posy - magnitude
-  } else if(action == 'E'){
-    posx <- posx + magnitude
-  } else if(action == 'W'){
-    posx <- posx - magnitude
-  } else if(action == 'L'){
-    r <- (r + magnitude) %% 360
-  } else if(action == 'R'){
-    r <- (r - magnitude) %% 360
-  } else {
-    posx <- posx + cos(2 * pi * r / 360) * magnitude
-    posy <- posy + sin(2 * pi * r / 360) * magnitude
+getManhattanDistance <- function(input, partTwo = F){
+  xtar <- 10
+  ytar <- 1
+  xpos <- 0
+  ypos <- 0
+  r <- 0
+  for(direction in input){
+    action <- unlist(strsplit(direction, split = ''))[1]
+    magnitude <- as.integer(paste(unlist(strsplit(direction, split = ''))[2:nchar(direction)], collapse = ''))
+    if(partTwo == F){
+      if(action == 'N'){
+        ypos <- ypos + magnitude
+      } else if(action == 'S'){
+        ypos <- ypos - magnitude
+      } else if(action == 'E'){
+        xpos <- xpos + magnitude
+      } else if(action == 'W'){
+        xpos <- xpos - magnitude
+      } else if(action == 'L'){
+        r <- (r + magnitude) %% 360
+      } else if(action == 'R'){
+        r <- (r - magnitude) %% 360
+      } else {
+        xpos <- xpos + cos(2 * pi * r / 360) * magnitude
+        ypos <- ypos + sin(2 * pi * r / 360) * magnitude
+      }
+    } else {
+      if(action == 'N'){
+        ytar <- ytar + magnitude
+      } else if(action == 'S'){
+        ytar <- ytar - magnitude
+      } else if(action == 'E'){
+        xtar <- xtar + magnitude
+      } else if(action == 'W'){
+        xtar <- xtar - magnitude
+      } else if(action == 'L'){
+        if(magnitude == 180){
+          xtar <- -xtar
+          ytar <- -ytar
+        } else if(magnitude == 90){
+          oldX <- xtar
+          oldY <- ytar
+          xtar <- -oldY
+          ytar <- oldX
+        } else {
+          oldX <- xtar
+          oldY <- ytar
+          xtar <- oldY
+          ytar <- -oldX
+        }
+      } else if(action == 'R'){
+        if(magnitude == 180){
+          xtar <- -xtar
+          ytar <- -ytar
+        } else if(magnitude == 90){
+          oldX <- xtar
+          oldY <- ytar
+          xtar <- oldY
+          ytar <- -oldX
+        } else {
+          oldX <- xtar
+          oldY <- ytar
+          xtar <- -oldY
+          ytar <- oldX
+        }
+      } else {
+        xpos <- xpos + xtar * magnitude
+        ypos <- ypos + ytar * magnitude
+      }
+    }
   }
+  return(abs(xpos) + abs(ypos))
 }
 
-abs(posx) + abs(posy)
+microbenchmark(getManhattanDistance(input))
 
 # part 2
+microbenchmark(getManhattanDistance(input, partTwo = T))
 
-xtar <- 10
-ytar <- 1
-xpos <- 0
-ypos <- 0
-for(direction in directions){
-  action <- unlist(strsplit(direction, split = ''))[1]
-  magnitude <- as.integer(paste(unlist(strsplit(direction, split = ''))[2:nchar(direction)], collapse = ''))
-  if(action == 'N'){
-    ytar <- ytar + magnitude
-  } else if(action == 'S'){
-    ytar <- ytar - magnitude
-  } else if(action == 'E'){
-    xtar <- xtar + magnitude
-  } else if(action == 'W'){
-    xtar <- xtar - magnitude
-  } else if(action == 'L'){
-    if(magnitude == 180){
-      xtar <- -xtar
-      ytar <- -ytar
-    } else if(magnitude == 90){
-      oldX <- xtar
-      oldY <- ytar
-      xtar <- -oldY
-      ytar <- oldX
-    } else {
-      oldX <- xtar
-      oldY <- ytar
-      xtar <- oldY
-      ytar <- -oldX
-    }
-  } else if(action == 'R'){
-    if(magnitude == 180){
-      xtar <- -xtar
-      ytar <- -ytar
-    } else if(magnitude == 90){
-      oldX <- xtar
-      oldY <- ytar
-      xtar <- oldY
-      ytar <- -oldX
-    } else {
-      oldX <- xtar
-      oldY <- ytar
-      xtar <- -oldY
-      ytar <- oldX
-    }
-  } else {
-    xpos <- xpos + xtar * magnitude
-    ypos <- ypos + ytar * magnitude
-  }
-  print(paste('move:', direction, 'xpos:', xpos, 'ypos:', ypos, 'xtar:', xtar, 'ytar:', ytar))
-}
-
-abs(xpos) + abs(ypos)
