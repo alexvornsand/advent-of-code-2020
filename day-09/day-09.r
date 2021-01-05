4d0b4da3ef380936021a892849df90fe976ed43c
alexvornsand <ajvornsand@ucdavis.edu>
2021-01-04 18:39:29 -0800

    Update day-09.r

--- day-09/day-09.r
+++ day-09/day-09.r
@@ -2,37 +2,52 @@
 # day 9
 
 # part 1
-numbers <- as.numeric(readLines('day-09.txt'))
+library(tictoc)
 
-findPair <- function(i, nums){
-  nums <- nums[(i-25):i]
-  for(j in 1:24){
-    for(k in (j+1):25){
-      if(nums[j] + nums[k] == nums[26]){
-        return(T)
+input <- readLines('day-09.txt')
+
+findEncription <- function(input, partTwo = F){
+  numbers <- as.numeric(input)
+  findPair <- function(i, nums){
+    nums <- nums[(i-25):i]
+    for(j in 1:24){
+      for(k in (j+1):25){
+        if(nums[j] + nums[k] == nums[26]){
+          return(T)
+        }
       }
     }
+    return(F)
   }
-  return(F)
+  targetNumber <- numbers[25 + min(which(unlist(sapply(26:length(numbers), findPair, nums = numbers)) == F))]
+  if(partTwo == F){
+    return(targetNumber)
+  } else {
+    findContiguousSum <- function(i, target){
+      sum <- numbers[i]
+      n <- i
+      while(sum < target){
+        n <- n + 1
+        sum <- sum + numbers[n]
+      }
+      if(sum == target){
+        key <- min(numbers[i:n]) + max(numbers[i:n])
+        return(key)
+      } else {
+        return(NA)
+      }
+    }
+    contiguousSums <- sapply(1:length(numbers), findContiguousSum, target = targetNumber)
+    contiguousSums <- contiguousSums[!is.na(contiguousSums)]
+    return(contiguousSums[1])
+  }
 }
 
-targetNumber <- numbers[25 + min(which(unlist(sapply(26:length(numbers), findPair, nums = numbers)) == F))]
+tictoc::tic()
+findEncription(input)
+tictoc::toc()
 
 # part 2
-
-findContiguousSum <- function(i, target = targetNumber){
-  sum <- numbers[i]
-  n <- i
-  while(sum < target){
-    n <- n + 1
-    sum <- sum + numbers[n]
-  }
-  if(sum == target){
-    key <- min(numbers[i:n]) + max(numbers[i:n])
-    print(key)
-  }
-}
-
-for(i in 1:560){
-  findContiguousSum(i)
-}
+tictoc::tic()
+findEncription(input, partTwo = T)
+tictoc::toc()
